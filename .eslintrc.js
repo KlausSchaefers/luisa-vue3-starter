79a2c69621be56e8898a7693e87a9efc61e4ee5d
KlausSchaefersAtWork <klaus.schaefers@philips.com>
2021-10-23 23:14:58 +0100

    Removed need to install css files

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'no-async-promise-executor': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Luisa from 'luisa-vue3'
-import 'luisa-vue3/dist/luisa-vue3.css'
 
 createApp(App).use(router).use(Luisa).mount('#app')
