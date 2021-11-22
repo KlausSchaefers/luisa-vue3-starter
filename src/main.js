744ecd27d2216d75ed22d06b94cd4d64db9b01a1
KlausSchaefersAtWork <klaus.schaefers@philips.com>
2021-11-22 22:28:48 +0000

    Update to HelloWorld

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import Luisa from 'luisa-vue'
 
-createApp(App).use(router).use(Luisa).mount('#app')
+createApp(App)
+    .use(router)
+    .use(Luisa)
+    .mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
- <Luisa :design="design" :config="config" v-model="viewModel" ref="qux"/>
+  <Luisa :design="design" :config="config" v-model="viewModel"/>
 </template>
 
 <script>
 /**
- * Here is the downloaded figma file. To download yours type in the command line
- * node download.js <FigmaToken> <FigmaFileId>  <Page1,Page2> (Optional only the pages you need)
+ * Here is the downloaded figma file. To download yours, type in the command line
+ * node download.js.
  */
 import app from './app.json'
 
@@ -13,9 +13,9 @@ export default {
   name: 'Home',
   data: function() {
     return {
-      design: app, // 'a2aa10aAzTPNq2B5yaDMREzIx9s5eeyyFsSqln5vtTft5dGoqy7wYJx9rJYO',
-      showViewModel: true,
+      design: app,
       viewModel: {
+        name:'',
       },
       config: {
       }
